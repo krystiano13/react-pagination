2154c69625827a2b067bf1a8ccc0399fb8f159fa
krystiano13 <crystianotv@gmail.com>
2023-09-24 20:14:57 +0200

    implement decreasing page

--- src/App.js
+++ src/App.js
@@ -2,24 +2,61 @@ import Wrapper from "./components/Wrapper";
 import Buttons from "./components/Buttons";
 import Items from "./components/Items";
 import { Card } from "./components/Card";
+import { useState, useEffect } from "react";
 
 const App = () => {
-    return (
-      <Wrapper className="bg-primary flex flex-col ai-center">
-        <Items className="flex jc-center ac-center">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-        </Items>
-        <Buttons className=""></Buttons>
-      </Wrapper>
-    );
-}
+  const [trueArray, setTrueArray] = useState([]);
+  const [page, setPage] = useState(1);
+
+  const previousPage = () => {
+    if (page - 1 >= 0) {
+      setPage((prev) => prev - 1);
+      showCards();
+    }
+    };
+    
+    
+
+  const showCards = () => {
+    const array = [0, 2, 3, 1, 1, 2, 0, 3, 2, 1, 1, 0, 0];
+    let dummyArray = [];
+    let last = array.length;
+
+    if ((page + 1) * 9 > array.length) {
+      last = array.length;
+    } else {
+      last = (page + 1) * 9;
+    }
+
+    dummyArray = array.slice(page * 9, last);
+    console.log(dummyArray);
+    setTrueArray(dummyArray);
+  };
+
+  useEffect(() => {
+    showCards();
+  }, [page]);
+
+  useEffect(() => {
+    console.log("changed");
+  }, [trueArray]);
+
+  return (
+    <Wrapper
+      id={page.toString()}
+      className="bg-primary flex flex-col ai-center"
+    >
+      <Items id={trueArray.length} className="flex jc-center ac-center">
+        {trueArray.map((item) => (
+          <Card colorId={item} />
+        ))}
+      </Items>
+      <Buttons className="">
+        <button onClick={previousPage}>prev</button>
+        <button>next</button>
+      </Buttons>
+    </Wrapper>
+  );
+};
 
 export { App };
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,29 @@
-import styled from "styled-components"
+import { useState, useEffect } from "react";
+import styled from "styled-components";
+import PropTypes from 'prop-types';
 
-export const Card = () => {
+export const Card = ({ colorId }) => {
+    const [color, setColor] = useState("bg-accent");
+
+    useEffect(() => { 
+        switch (colorId) {
+            case 0:
+                setColor("bg-accent");
+                break;
+            case 1:
+                setColor("bg-secondary");
+                break;
+            case 2:
+                setColor("bg");
+                break;
+            case 3:
+                setColor("bg-red");
+                break;
+        }
+    }, []);
+
     return (
-        <CardWrapper className="br-1 bg-accent c-pointer m-3" />
+        <CardWrapper className={`br-1 ${color} c-pointer m-3`} />
     )
 }
 
@@ -11,3 +32,6 @@ const CardWrapper = styled.div`
     height : 6rem;
 `;
 
+Card.propTypes = {
+    colorId : PropTypes.number
+}
